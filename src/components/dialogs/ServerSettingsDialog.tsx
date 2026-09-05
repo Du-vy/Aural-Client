278b486802cbcb9e7c62c96197c32556512edd69
Duvy <200692241+Du-vy@users.noreply.github.com>
2026-09-04 23:08:55 -0300

    chore: bump version to 0.8.5

--- src/components/dialogs/ServerSettingsDialog.tsx
+++ src/components/dialogs/ServerSettingsDialog.tsx
@@ -207,7 +207,7 @@ export function ServerSettingsDialog({
         </span>
       </button>
       <div className="settings-sidebar__version-wrap">
-        <span className="settings-sidebar__version">Aural Client v0.8.3</span>
+        <span className="settings-sidebar__version">Aural Client v0.8.5</span>
       </div>
     </div>
   );
