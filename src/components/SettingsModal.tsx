609cbc9b914be8319d436ee5d5e6c08c1de25968
Duvy <200692241+Du-vy@users.noreply.github.com>
2026-09-17 00:58:41 -0300

    chore: bump version to 0.9.3

--- src/components/SettingsModal.tsx
+++ src/components/SettingsModal.tsx
@@ -131,7 +131,7 @@ export function SettingsModal({
             <div className="settings-sidebar__footer">{sidebarFooter}</div>
           ) : (
             <div className="settings-sidebar__footer">
-              <span className="settings-sidebar__version">Aural Client v0.9.2</span>
+              <span className="settings-sidebar__version">Aural Client v0.9.3</span>
             </div>
           )}
         </aside>
